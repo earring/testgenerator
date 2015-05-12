2323e90c96bf1cd8a853947aab7f38b587d78be1
earring <artemonische@yandex.ru>
2015-05-12 19:17:49 +0300

    #28 правки в форме добавления вопроса

--- src/main/java/ru/earrring/testgenerator/frames/AddingFrame.java
+++ src/main/java/ru/earrring/testgenerator/frames/AddingFrame.java
@@ -8,44 +8,52 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddingFrame extends AFrame {
+
+    private final static int MAX_VARIANT = 15;
+
     private int gridy = 0;
     private int answersGridy = 0;
-    private JLabel descriptionLabel;
     private JLabel addVariantLabel;
     private JTextArea descriptionArea;
     private JPanel answerWidgets;
+    private JScrollPane answerScrollPane;
     private JButton addAnswerButton;
     private JPanel mainPanel;
 
     protected void adjustFrameSettings() {
         setTitle("Добавить вопрос");
-        setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);
-        setResizable(true);
+        setSize(700, 700);
+        setResizable(false);
     }
 
     protected void adjustLayout() {
         mainPanel = new JPanel();
-        setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL;
-
-        JScrollPane mainScrollPane = new JScrollPane(mainPanel);
-        add(mainScrollPane, c);
+        setLayout(new BorderLayout());
+        add(mainPanel, BorderLayout.CENTER);
 
         mainPanel.setLayout(new GridBagLayout());
-        descriptionLabel = new JLabel("Введите текст вопроса:");
-        descriptionArea = new JTextArea("В чем смысл жизни?", 5, 30);
+
+        descriptionArea = new JTextArea(5, 50);
+        JScrollPane descriptionScrollPane = new JScrollPane(descriptionArea);
+        descriptionArea.setToolTipText("Введите текст вопроса:");
+        descriptionArea.setFont(new Font("Serif", Font.ITALIC, 16));
+        descriptionArea.setLineWrap(true);
+        descriptionArea.setWrapStyleWord(true);
+        descriptionScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        descriptionScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        addComponentToMainPanel(descriptionScrollPane, 0, 0, 1.5);
+
+        addAnswerButton = new JButton("Добавить вариант ответа");
+        addAnswerButton.setFont(new Font("Arial", Font.PLAIN, 22));
+        addComponentToMainPanel(addAnswerButton, 0, 1, 0.2);
 
         answerWidgets = new JPanel();
         answerWidgets.setLayout(new GridBagLayout());
-        JScrollPane pane = new JScrollPane(answerWidgets);
+        answerScrollPane = new JScrollPane(answerWidgets);
 
-        addAnswerButton = new JButton("Добавить вариант ответа");
-
-        addComponent(descriptionLabel, 0, 0);
-        addComponent(descriptionArea, 0, 1);
-        addComponent(pane, 0, 2);
-        addComponent(addAnswerButton, 0, 3);
+        answerScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        answerScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        addComponentToMainPanel(answerScrollPane, 0, 2, 2.5);
     }
 
     /**
@@ -56,6 +64,9 @@ public class AddingFrame extends AFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 addAnswerWidget();
+                if (answersGridy == MAX_VARIANT) {
+                    addAnswerButton.setEnabled(false);
+                }
             }
         });
     }
@@ -65,9 +76,12 @@ public class AddingFrame extends AFrame {
         setVisible(false);
     }
 
-    private void addComponent(JComponent component, int gridx, int gridy) {
+    private void addComponentToMainPanel(JComponent component, int gridx, int gridy, double weighty) {
         GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.NORTH;
+        c.weightx = 1.0;
+        c.weighty = weighty;
         c.gridx = gridx;
         c.gridy = gridy;
         mainPanel.add(component, c);
@@ -77,18 +91,26 @@ public class AddingFrame extends AFrame {
         JLabel answerLabel = new JLabel(String.format("%d)   ", answersGridy + 1));
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.NORTH;
+        c.weightx = 0.1;
+        c.weighty = 1.0;
         c.gridx = 0;
         c.gridy = answersGridy;
+        answerLabel.setFont(new Font("Arial", Font.PLAIN, 22));
         answerWidgets.add(answerLabel, c);
 
         AddingComponent newWidget = new AddingComponent();
         c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.NORTH;
+        c.weightx = 1.0;
+        c.weighty = 1.0;
         c.gridx = 1;
         c.gridy = answersGridy;
         answerWidgets.add(newWidget, c);
 
         answersGridy = answersGridy + 1;
 
-        validate();
+        answerScrollPane.revalidate();
+        answerScrollPane.repaint();
     }
 }
